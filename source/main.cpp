d80b78f3e9484aa7934b11d566d4f683fe94a99a
vinibalvarenga <alvarenga.vinicius@hotmail.com>
2023-12-19 16:08:12 +0100

    question 5

--- source/main.cpp
+++ source/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    // Cria uma instância da classe Photo
-    Photo* photo = new Photo("Foto1", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/photos/drosil.jpg", 40.7128, 74.0060);
-    photo->display(cout);
-    photo->play();
+   
+   const int NUM_ITEMS = 4;
+    Multimedia* items[NUM_ITEMS];
 
-    // Cria uma instância da classe Video
-    Video* video = new Video("Video1", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/Videos/video.jpeg", 120);
-    video->display(cout);
-    video->play();
+    items[0] = new Photo("Foto1", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/photos/drosil.jpg", 40.7128, 74.0060);
+    items[1] = new Video("Video1", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/videos/video1.mp4", 120);
+    items[2] = new Photo("Foto2", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/photos/vini.jpg", 40.7128, 50);
+    items[3] = new Video("Video2", "/Users/vinibalvarenga/Documentos/Telecom/1A/Creneau_D/Paradigmes/tp1/videos/video2.mp4", 120);
 
-    // Não se esqueça de deletar os objetos quando terminar de usá-los
-    delete photo;
-    delete video;
+    for (int i = 0; i < NUM_ITEMS; i++) {
+        items[i]->display(cout);
+        items[i]->play();
+    }
+
+    for (int i = 0; i < NUM_ITEMS; i++) {
+        delete items[i];
+    }
 
     return 0;
 }
